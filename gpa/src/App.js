5a2fcc816d4773d7aeefae07bfc03ad6097458d2
gmxmayank <mayank.dwivedi@gyanmatrix.com>
2019-03-16 14:03:06 +0530

    CREATED FORM With dynamic data

--- gpa/src/App.js
+++ gpa/src/App.js
@@ -1,25 +1,36 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+
+// import { Button } from "react-bootstrap";
+import AppHeader from "./component/app.header";
+import StudentForm from "./component/student.form";
+
+import Grid from "@material-ui/core/Grid";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Grid container spacing={24}>
+          <Grid item xs={12}>
+            <AppHeader />
+          </Grid>
+          <Grid item xs={4} className="formFields">
+            <StudentForm name="Student Name" type="text" fw />
+          </Grid>
+          <Grid item xs={4} className="formFields">
+            <StudentForm name="Parents/Guardian Name" type="text" fw />
+          </Grid>
+          <Grid item xs={4} className="formFields">
+            <StudentForm name="Email id" type="email" fw />
+          </Grid>
+          <Grid item xs={4} className="formFields">
+            <StudentForm name="Phone Number" type="number" fw />
+          </Grid>
+          <Grid item xs={4} className="formFields">
+            <StudentForm name="NEET Score:" type="number" fw />
+          </Grid>
+        </Grid>
       </div>
     );
   }
